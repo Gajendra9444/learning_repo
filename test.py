00396f2046d8f4b406627f285edd653b282582c1
Gajendra9444 <gajendrakunwar9444@gmail.com>
2025-06-05 07:08:33 +0545

    added new branch for coding

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
 print("no worries")
+
+# git clone "repo-url" -- in ssh
+# open the git repo folder in s code 
+# changes are done in the repo folder 
+# after we done with changes -- git add or git add files
+# after add -- git commit -m "relavent message"
+# noe update changes in remote
+# -- git push origin main  
