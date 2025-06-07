985d06335df9927f352c610c0db70a0c05b957a3
Gajendra9444 <gajendrakunwar9444@gmail.com>
2025-06-07 10:23:06 +0545

    this is committed

---
+++ gajju.py
@@ -0,0 +1,18 @@
+print("my Name is Gajendra")
+print("i am studying at Broadways")
+
+
+# git checkout -b branch_name
+# to checkout or create a new branch
+# git checkout branch_name 
+# to checkout to an existing branch
+# do some changes on the branch 
+#git add .
+# git commit -m "relevant message"
+#git push origin branch_name 
+# to pull changes from branch
+# git pull origin branch_name 
+
+# if mege message shows up 
+# if vim do :q or :q!
+# if nano editor do ctrl + X
